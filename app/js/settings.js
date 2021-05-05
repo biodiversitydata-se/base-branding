3410e54e247bccea8a00c10d19930194e1c5b68d
shahmanash <manash.shah@gmail.com>
2021-05-05 15:47:07 +0200

    customizing for sbdi and update URLs

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,38 +1,38 @@
 module.exports = {
   isDevel: true,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
-  enabledLangs: ["en", "es", "zh", "sw"],
-  mainDomain: "l-a.site", // used for cookies (without http/https)
-  mainLAUrl: "https://l-a.site",
-  baseFooterUrl: "https://l-a.site",
-  theme: "simplex", // for now 'material', 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
+  enabledLangs: ["en"],
+  mainDomain: "static.biodiversitydata.se", // used for cookies (without http/https)
+  mainLAUrl: "https://static.biodiversitydata.se",
+  baseFooterUrl: "https://static.biodiversitydata.se",
+  theme: "sbdi", // for now 'material', 'sbdi' 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
   services: {
-    collectory: { url: "https://collections.l-a.site", title: "Collections" },
-    biocache: { url: "https://biocache.l-a.site", title: "Occurrence records" },
+    collectory: { url: "https://collections.biodiversitydata.se", title: "Collections" },
+    biocache: { url: "https://records.biodiversitydata.se", title: "Occurrence records" },
     biocacheService: {
-      url: "https://biocache-ws.l-a.site",
+      url: "https://records.biodiversitydata.se/ws",
       title: "Occurrence records webservice",
     },
-    bie: { url: "https://species.l-a.site", title: "Species" },
+    bie: { url: "https://species.biodiversitydata.se", title: "Species" },  
     // This bieService var is used by the search autocomplete. With your BIE
     // service running and well indexed, you can put here your url
     bieService: {
-      url: "https://bie.ala.org.au/ws",
+      url: "https://species.biodiversitydata.se/ws",
       title: "Species webservice",
     },
-    regions: { url: "https://regions.l-a.site", title: "Regions" },
-    lists: { url: "https://lists.l-a.site", title: "Species List" },
-    spatial: { url: "https://spatial.l-a.site", title: "Spatial Portal" },
-    images: { url: "https://images.l-a.site", title: "Images Service" },
-    cas: { url: "https://auth.l-a.site", title: "CAS" },
+    regions: { url: "https://regions.biodiversitydata.se", title: "Regions" },
+    lists: { url: "https://lists.biodiversitydata.se", title: "Species List" },
+    spatial: { url: "https://spatial.biodiversitydata.se", title: "Spatial Portal" },
+    images: { url: "https://images.biodiversitydata.se", title: "Images Service" },
+    cas: { url: "https://auth.biodiversitydata.se", title: "CAS" },
   },
   otherLinks: [
-    { title: "Datasets", url: "https://collections.l-a.site/datasets" },
+    { title: "Datasets", url: "https://collections.biodiversitydata.se/datasets" },
     {
       title: "Explore your area",
-      url: "http://biocache.l-a.site/explore/your-area/",
+      url: "http://records.biodiversitydata.se/explore/your-area/",
     },
-    { title: "Datasets", url: "https://collections.l-a.site/datasets" },
+    { title: "Datasets", url: "https://collections.biodiversitydata.se/datasets" },
     { title: "twitter", url: "", icon: "twitter" },
   ],
 };
