4e6a4f4d15729fe12de3dcc5f3477b25e56b7bdb
Mats Bovin <mats.bovin@gmail.com>
2025-08-22 10:28:26 +0200

    Replace static subdomain with bioatlas

--- app/js/settings.js
+++ app/js/settings.js
@@ -2,9 +2,9 @@ module.exports = {
   isDevel: true,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
   enabledLangs: ["en"],
-  mainDomain: "static.biodiversitydata.se", // used for cookies (without http/https)
-  mainLAUrl: "https://static.biodiversitydata.se",
-  baseFooterUrl: "https://static.biodiversitydata.se",
+  mainDomain: "bioatlas.biodiversitydata.se", // used for cookies (without http/https)
+  mainLAUrl: "https://bioatlas.biodiversitydata.se",
+  baseFooterUrl: "https://bioatlas.biodiversitydata.se",
   theme: "sbdi", // for now 'material', 'sbdi' 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
   services: {
     collectory: { url: "https://collections.biodiversitydata.se", title: "Collections" },
