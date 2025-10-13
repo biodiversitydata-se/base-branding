4ecd687aa83aa572df91df0ef1ab5983a0b71f55
Mats Bovin <mats.bovin@gmail.com>
2025-10-13 13:03:12 +0200

    Update prod settings

--- app/js/settings-prod.js
+++ app/js/settings-prod.js
@@ -1,38 +1,36 @@
-module.exports = {
+export default {
   isDevel: false,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
-  enabledLangs: ["en"],
-  mainDomain: "biodiversitydata.se", // used for cookies (without http/https)
-  mainLAUrl: "https://bioatlas.biodiversitydata.se",
-  baseFooterUrl: "https://bioatlas.biodiversitydata.se",
-  theme: "sbdi", // for now 'material', 'sbdi' 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
+  enabledLangs: ['en'],
+  mainDomain: 'biodiversitydata.se', // used for cookies (without http/https)
+  mainLAUrl: 'https://bioatlas.biodiversitydata.se',
+  baseFooterUrl: 'https://bioatlas.biodiversitydata.se',
+  theme: 'sbdi', // for now 'material', 'sbdi' 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
   services: {
-    collectory: { url: "https://collections.biodiversitydata.se", title: "Collections" },
-    biocache: { url: "https://records.biodiversitydata.se", title: "Occurrence records" },
+    collectory: { url: 'https://collections.biodiversitydata.se', title: 'Collections' },
+    biocache: { url: 'https://records.biodiversitydata.se', title: 'Occurrence records' },
     biocacheService: {
-      url: "https://records.biodiversitydata.se/ws",
-      title: "Occurrence records webservice",
+      url: 'https://records.biodiversitydata.se/ws',
+      title: 'Occurrence records webservice',
     },
-    bie: { url: "https://species.biodiversitydata.se", title: "Species" },  
-    // This bieService var is used by the search autocomplete. With your BIE
-    // service running and well indexed, you can put here your url
+    bie: { url: 'https://species.biodiversitydata.se', title: 'Species' },  
     bieService: {
-      url: "https://species.biodiversitydata.se/ws",
-      title: "Species webservice",
+      url: 'https://species.biodiversitydata.se/ws',
+      title: 'Species webservice',
     },
-    regions: { url: "https://regions.biodiversitydata.se", title: "Regions" },
-    lists: { url: "https://lists.biodiversitydata.se", title: "Species List" },
-    spatial: { url: "https://spatial.biodiversitydata.se", title: "Spatial Portal" },
-    images: { url: "https://images.biodiversitydata.se", title: "Images Service" },
-    cas: { url: "https://auth.biodiversitydata.se", title: "CAS" },
+    regions: { url: 'https://regions.biodiversitydata.se', title: 'Regions' },
+    lists: { url: 'https://lists.biodiversitydata.se', title: 'Species List' },
+    spatial: { url: 'https://spatial.biodiversitydata.se', title: 'Spatial Portal' },
+    images: { url: 'https://images.biodiversitydata.se', title: 'Images Service' },
+    cas: { url: 'https://auth.biodiversitydata.se', title: 'CAS' },
   },
   otherLinks: [
-    { title: "Datasets", url: "https://collections.biodiversitydata.se/datasets" },
+    { title: 'Datasets', url: 'https://collections.biodiversitydata.se/datasets' },
     {
-      title: "Explore your area",
-      url: "http://records.biodiversitydata.se/explore/your-area/",
+      title: 'Explore your area',
+      url: 'http://records.biodiversitydata.se/explore/your-area/',
     },
-    { title: "Datasets", url: "https://collections.biodiversitydata.se/datasets" },
-    { title: "twitter", url: "", icon: "twitter" },
+    { title: 'Datasets', url: 'https://collections.biodiversitydata.se/datasets' },
+    { title: 'twitter', url: '', icon: 'twitter' },
   ],
 };
