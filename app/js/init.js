e626d3ac44488b421f6789ccec31cffae2362772
Mats Bovin <mats.bovin@gmail.com>
2026-09-23 10:45:24 +0200

    Remove counters

--- app/js/init.js
+++ app/js/init.js
@@ -2,6 +2,5 @@ import './settings.js';
 import './index-auth.js';
 import './i18next-config.js';
 import './mante.js';
-import './stats.js';
 import './autocomplete-conf.js';
 import 'url-polyfill'; // for URL() usage in older browsers
